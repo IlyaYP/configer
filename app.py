15cb2c7cddd0720f7f058ae1175baf4a8f757cff
Ilya Suprun <suprunis@aes.tgc-2.ru>
2022-10-17 14:51:41 +0300

    modified:   app.py

--- app.py
+++ app.py
@@ -3,12 +3,18 @@ import shutil
 from os import listdir
 from os.path import isfile, join, getmtime
 from git import Repo
+import logging
 
-#os.path.getmtime(path)
+#numeric_level = getattr(logging, loglevel.upper(), None)
+#if not isinstance(numeric_level, int):
+#    raise ValueError('Invalid log level: %s' % loglevel)
+
+logging.basicConfig(filename='/var/log/configer.log', filemode='w', 
+        format='%(asctime)s %(message)s', level=logging.INFO)
 
 mypath = "/home/ftp/cfg"
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-#print(sorted(onlyfiles))
+#logging.debug(f'Values {sorted(onlyfiles)}')
 
 
 hostname = re.compile('(.*[A-z0-9])--?([A-Za-z]{3}--?\d\d?-202\d)-(\d\d-\d\d-\d\d)-MSK-(\d+)$',re.IGNORECASE)
@@ -17,7 +23,7 @@ configs = {}
 for file in onlyfiles:
     m = hostname.match(file)
     if m:
-        #print(m.group(1), m.group(2),m.group(3), m.group(4))
+        logging.debug(f'Values {m.group(1), m.group(2),m.group(3), m.group(4)}')
         config = {
                 'file': file,
                 'hostname': m.group(1),
@@ -30,24 +36,23 @@ for file in onlyfiles:
             if int(config['time']) > int(configs[config['hostname']]['time']):
                 configs[config['hostname']] = config
     else:
-        print("doesn't match",file)
+        logging.warning(f"Doesn't match, {file}")
 
 for key in configs:
     src = join(mypath, configs[key]['file'])
     dst = join(mypath, 'rep', configs[key]['hostname'])
     shutil.copyfile(src, dst)
-    #print(dst)
+    logging.debug(dst)
 
 repo = Repo(join(mypath, 'rep'))
 if len(repo.untracked_files) > 0:  # retrieve a list of untracked files
-    #repo.git.add(".")
     repo.index.add(repo.untracked_files)
 
 if repo.is_dirty():  # check the dirty state
-    #repo.git.commit('-am', 'script commit')
     repo.git.add(update=True)
     repo.index.commit('script commit')
     origin = repo.remote('origin')
     origin.push()
-    #print('dirty')
-
+    logging.info('dirty')
+else:
+    logging.info('clean')
