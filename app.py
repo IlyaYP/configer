f64340864ab5bdf893a0b4ad8afff2e40247bedd
Ilya Suprun <suprunis@aes.tgc-2.ru>
2024-01-11 18:19:46 +0300

    On branch main  Your branch is up to date with 'github/main'.
     Changes to be committed:
    	modified:   app.py

--- app.py
+++ app.py
@@ -17,18 +17,21 @@ onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 #logging.debug(f'Values {sorted(onlyfiles)}')
 
 
-hostname = re.compile('(.*[A-z0-9])-[-\.]?([A-Za-z]{3}--?\d\d?-202\d)-(\d\d[-:]\d\d[-:]\d\d)-MSK-(\d+)$',
-                      re.IGNORECASE)
+#hostname = re.compile('(.*[A-z0-9])-[-\.]?([A-Za-z]{3}--?\d\d?-202\d)-(\d\d[-:]\d\d[-:]\d\d)-MSK-(\d+)$',
+#                      re.IGNORECASE)
+hostname = re.compile('(.*?-.*?)-', re.IGNORECASE)
+
 configs = {}
 
 for file in onlyfiles:
     m = hostname.match(file)
     if m:
-        logging.debug(f'Values {m.group(1), m.group(2),m.group(3), m.group(4)}')
+        #logging.debug(f'Values {m.group(1), m.group(2),m.group(3), m.group(4)}')
+        logging.debug(f'Values {m.group(1)}')
         config = {
                 'file': file,
                 'hostname': m.group(1),
-                'index': m.group(4),
+                #'index': m.group(4),
                 'time': getmtime(join(mypath, file))
         }
         if configs.get(config['hostname']) == None:
@@ -39,12 +42,14 @@ for file in onlyfiles:
     else:
         logging.warning(f"Doesn't match, {file}")
 
+# copy to repo dir
 for key in configs:
     src = join(mypath, configs[key]['file'])
     dst = join(mypath, 'rep', configs[key]['hostname'])
     shutil.copyfile(src, dst)
     logging.debug(dst)
 
+# add, commit and push changes to repo
 repo = Repo(join(mypath, 'rep'))
 if len(repo.untracked_files) > 0:  # retrieve a list of untracked files
     repo.index.add(repo.untracked_files)
@@ -57,3 +62,4 @@ if repo.is_dirty():  # check the dirty state
     logging.info('dirty')
 else:
     logging.info('clean')
+
